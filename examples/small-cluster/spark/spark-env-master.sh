60e54ec9b030d8e051c019d05fd7bad0c7bace0d
Leonid Isaev <leonid.isaev@jila.colorado.edu>
2019-09-26 14:24:03 -0400

    Add Apache Spark buildscripts and example configuration.

---
+++ examples/small-cluster/spark/spark-env-master.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export JAVA_HOME=/usr/lib/jvm/default-runtime
+
+if (command -v hadoop 2> /dev/null); then
+    export SPARK_DIST_CLASSPATH="$(hadoop classpath)"
+else
+    export SPARK_DIST_CLASSPATH=""
+fi
+
+export SPARK_MASTER_HOST=spark-node1 \
+        SPARK_LOCAL_IP=192.168.122.81
